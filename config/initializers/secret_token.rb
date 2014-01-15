9d5b560b26ab61bf2d2ef4585a499941095f5d13
Ruslan Kenzhebekov <reke.essential@gmail.com>
2014-01-16 00:52:12 +0600

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lingvo::Application.config.secret_key_base = '68907313921c8cc4b0e3bedaafc8a1dfd1a2178f12b61b763f51b05e87c7b5f29cd3cbd8708734e8fd60b909ccedbb7588aedba7434e4cc69d70b10a52ebeccd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lingvo::Application.config.session_store :cookie_store, key: '_lingvo_session'
